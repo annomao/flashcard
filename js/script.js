be55775a448a35297a0ba9c9e5d493e0179522b8
annomao <omaoannkerubo@gmail.com>
2022-03-10 12:43:48 +0300

    add functions to show and hide div

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-$(document).ready(function() {
-  $("#js").click(function() {
-    $("#js-def").show();
-    $("#js").hide();
-  });
 
-  $("#js-def").click(function() {
-    $("#js").show();
-    $("#js-def").hide();
+jQuery(document).ready(function($) {
+  $('h3').click(function() {
+    var index = $('h3').index(this);
+    $("p").eq(index).show();
+    $("h3").eq(index).hide();
+  });
+  $('p').click(function() {
+    var index = $('p').index(this);
+    $("h3").eq(index).show();
+    $("p").eq(index).hide();
   });
 });
